44ab2fd4f2b68ecf1901d4cd1370cc966fe396bf
Tom Wyant <wyant@cpan.org>
2019-10-30 11:21:47 -0400

    Version 0.004
    Set version to 0.004. Update Changes file for changes since v00.003_03.

--- inc/Mock/HTTP/Tiny.pm
+++ inc/Mock/HTTP/Tiny.pm
@@ -8,7 +8,7 @@ use warnings;
 use Carp;
 use Storable ();
 
-our $VERSION = '0.003_03';
+our $VERSION = '0.004';
 
 use constant HASH_REF	=> ref {};
 
--- lib/Test/Pod/LinkCheck/Lite.pm
+++ lib/Test/Pod/LinkCheck/Lite.pm
@@ -22,7 +22,7 @@ use Scalar::Util ();		# Core since 5.7.3
 use Storable ();		# Core since 5.7.3
 use Test::Builder ();		# Core since 5.6.2
 
-our $VERSION = '0.003_03';
+our $VERSION = '0.004';
 
 our @ISA = qw{ Exporter };
 
