348bd8de76d95d1bfd2f7a80142edfbae80dbf25
Tom Wyant <wyant@cpan.org>
2022-09-04 10:39:01 -0400

    Version 0.008_02
    Set version to 0.008_02. Update Changes file for changes since v00.008_01.

--- inc/Mock/HTTP/Tiny.pm
+++ inc/Mock/HTTP/Tiny.pm
@@ -8,7 +8,7 @@ use warnings;
 use Carp;
 use Storable ();
 
-our $VERSION = '0.008_01';
+our $VERSION = '0.008_02';
 
 use constant HASH_REF	=> ref {};
 
--- t/data/_lib/My/Test/Module.pm
+++ t/data/_lib/My/Test/Module.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use Carp;
 
-our $VERSION = '0.008_01';
+our $VERSION = '0.008_02';
 
 
 1;
